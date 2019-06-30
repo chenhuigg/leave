bb5f3b102fbd6b1ddd0fc37f3d000c36dd067876
chenhuigg <2466811749@qq.com>
2019-06-30 13:59:17 +0800

    修复了角色列表无法显示的问题

--- src/main/java/cn/edu/pdsu/controller/PremissionController.java
+++ src/main/java/cn/edu/pdsu/controller/PremissionController.java
@@ -1,17 +1,10 @@
 package cn.edu.pdsu.controller;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import cn.edu.pdsu.pojo.AjaxResult;
-import cn.edu.pdsu.pojo.Premission;
-import cn.edu.pdsu.service.PremissionService;
 
 @RestController
 public class PremissionController {
-	private PremissionService premissionService;
+	//private PremissionService premissionService;
 	
 	
 
